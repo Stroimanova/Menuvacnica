53ffb8e8b504baed1a6114796c3d8e79fcb957a7
unknown <simonastroimanova@hotmail.com>
2024-01-20 18:29:50 +0100

    first java program

---
+++ src/proekt56/Menuvacnica.java
@@ -0,0 +1,10 @@
+package proekt56;
+
+public class Menuvacnica {
+	public static void main(String[] args) {
+		int a=65;
+		double x=a*61.7;
+		System.out.println("65evra vo denari se: ");
+		System.out.println(x);
+		}
+}
